29a3ec9bbf16e71ef49d2ff2564008aa584938f2
ELi1012 <amli1@ualberta.ca>
2026-04-02 14:38:32 -0600

    cronjob pa services

---
+++ srv/images/cron-provider1/srv/services/purpleair/consts.ts
@@ -0,0 +1,26 @@
+const LOCATION_TYPE_OUTSIDE = 0
+
+const METADATA_FIELDS="name,latitude,longitude";
+
+
+const ALBERTA_BBOX_COORDINATES = {
+    nwlat: 53.7158,
+    nwlng: -113.7945,
+    selat: 53.3690,
+    selng: -112.6420,
+}
+
+const EDMONTON_BBOX_COORDINATES = {
+    nwlat: 53.7158,
+    nwlng: -113.7945,
+    selat: 53.3690,
+    selng: -112.6420,
+}
+
+
+export {
+    LOCATION_TYPE_OUTSIDE,
+    METADATA_FIELDS,
+    ALBERTA_BBOX_COORDINATES,
+    EDMONTON_BBOX_COORDINATES
+}
--- srv/images/cron-provider1/srv/services/purpleair/ingest.ts
+++ srv/images/cron-provider1/srv/services/purpleair/ingest.ts
@@ -1,39 +1,15 @@
 import { URLSearchParams } from "url";
 import { _read_data, _write_data } from "../../utils"
 
-import { pool } from "../../db/pools"
-import format from "pg-format"
+import { fetch_from_url } from "../utils"
+import { LOCATION_TYPE_OUTSIDE, METADATA_FIELDS, EDMONTON_BBOX_COORDINATES } from "./consts"
 
-// const client = new Client({
-//   connectionString: process?.env?.DATABASE_URL || "postgresql://postgres:postgres@localhost:5432/air_quality_data"
-// });
 
-const METADATA_FIELDS="name,latitude,longitude";
 // const PURPLE_AIR_FIELDS="name,last_seen,pm2.5_10minute,pm2.5_30minute,pm2.5_60minute,pm2.5_6hour,pm2.5_24hour,latitude,longitude,humidity";
 const READING_FIELDS="last_seen,pm2.5_10minute,pm2.5_30minute,pm2.5_60minute,pm2.5_6hour,pm2.5_24hour,humidity";
-const LOCATION_TYPE_OUTSIDE = 0;
-const EDMONTON_BBOX_COORDINATES = {
-    nwlat: 53.7158,
-    nwlng: -113.7945,
-    selat: 53.3690,
-    selng: -112.6420,
-}
 
 const { PA_READ_KEY, PA_GROUP_ID } = process.env;
 
-async function fetch_from_url(url: string, headers: Record<string, any>) {
-    const response = await fetch(url, {
-        method: "GET",
-        headers
-    });
-
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    return data;
-
-}
 
 export async function get_purpleair_sensor_data() {
     if (PA_READ_KEY === undefined) throw new Error("PURPLEAIR READ KEY NOT PROVIDED");
