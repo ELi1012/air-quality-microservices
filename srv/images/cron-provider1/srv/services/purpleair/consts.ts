26206c3eab4c66ff6ba02aee6cd811f48cc06159
ELi1012 <amli1@ualberta.ca>
2026-04-14 16:55:27 -0600

    update alberta bbox coordinates

--- srv/images/cron-provider1/srv/services/purpleair/consts.ts
+++ srv/images/cron-provider1/srv/services/purpleair/consts.ts
@@ -4,10 +4,10 @@ const METADATA_FIELDS="name,latitude,longitude";
 
 
 const ALBERTA_BBOX_COORDINATES = {
-    nwlat: 53.7158,
-    nwlng: -113.7945,
-    selat: 53.3690,
-    selng: -112.6420,
+    nwlat: 60,
+    nwlng: -120,
+    selat: 49,
+    selng: -110,
 }
 
 const EDMONTON_BBOX_COORDINATES = {
